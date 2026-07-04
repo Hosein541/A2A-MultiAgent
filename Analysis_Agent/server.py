bda9ceac56ccd0c212f0b3a07244a27bce99e000
Hosein541 <huseinmohammadi83@gmail.com>
2026-07-04 13:28:36 +0330

    delete additional codes from analysis server

--- Analysis_Agent/server.py
+++ Analysis_Agent/server.py
@@ -1,57 +1,8 @@
-import asyncio
-
-from python_a2a import (
-    AgentCard,
-    AgentSkill,
-    run_server,
-)
-from python_a2a.langchain import to_a2a_server
-
-# from agent import search_agent
-
-
-# def build_card():
-
-#     return AgentCard(
-#         name="Search Agent",
-#         description="AI agent specialized in web search using MCP tools.",
-#         url="http://localhost:8001",
-#         version="1.0.0",
-#         skills=[
-#             AgentSkill(
-#                 name="web_search",
-#                 description="Search internet and return structured results.",
-#                 tags=["search", "web", "tavily"],
-#             )
-#         ],
-#         capabilities={
-#             "streaming": True,
-#         },
-#     )
-
-
-# async def main():
-
-#     await search_agent.initialize()
-
-#     card = build_card()
-
-#     a2a_server = to_a2a_server(
-#         search_agent.agent,
-#         agent_card=card   # 🔥 مهم‌ترین fix
-#     )
-
-#     run_server(
-#         a2a_server,
-#         host="0.0.0.0",
-#         port=8001,
-#     )
-
-import asyncio
-from python_a2a import AgentCard, AgentSkill, run_server, A2AServer
 from agent import AnalysisA2AServer
+import asyncio
+from python_a2a.langchain import to_a2a_server
+from python_a2a import AgentCard, AgentSkill, run_server, A2AServer
 
-from python_a2a import AgentCard, AgentSkill
 
 def build_card():
     """Build AgentCard for the Analysis Agent (Market + Calculator tools)"""
@@ -132,24 +83,7 @@ def build_card():
     )
 
 async def main():
-    # await analysis_agent.initialize()   # فعلاً کامنت
 
-
-    # a2a_server = A2AServer(analysis_agent)
-    # a2a_server.agent_card = build_card()
-
-    # print("✅ AgentCard attached!")
-    # print(f"Name : {a2a_server.agent_card.name}")
-    # print(f"URL  : {a2a_server.agent_card.url}")
-
-    # print("\n🚀 Starting A2A Server...")
-    # run_server(a2a_server, host="0.0.0.0", port=8002)
-    # a2a_server = AnalysisA2AServer()
-
-    # await a2a_server.analysis_agent.initialize()   # اگر داخل __init__ انجام ندادی
-    # await a2a_server.analysis.initialize()
-
-    # a2a_server.agent_card = build_card()
     card = build_card()
 
     a2a_server = AnalysisA2AServer(card)
@@ -161,14 +95,6 @@ async def main():
         host="0.0.0.0",
         port=8002,
     )
-
-    # print("✅ AgentCard attached!")
-    # print(f"Name : {a2a_server.agent_card.name}")
-    # print(f"URL  : {a2a_server.agent_card.url}")
-# 
-    # print("\n🚀 Starting A2A Server...")
-    # run_server(a2a_server, host="0.0.0.0", port=8002)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
